4893b587f5a1559a96c4d1c066138d219c2e4b91
sa-lda <85459043+sa-lda@users.noreply.github.com>
2023-05-28 01:32:38 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const cors = require('cors');
+const mysql = require('mysql');
 
 const app = express();
 app.use(cors({
@@ -9,9 +10,42 @@ app.use(cors({
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
+  
+const db = mysql.createConnection({
+    host: "aws.connect.psdb.cloud",
+    port: 3306,
+    user: "jbhbvlo7mnmdkxdflfoa",
+    password: "pscale_pw_AJvaq9u0WV1ThxQyypEQMA4CQ7mAoWlZdKH33syj2OK",
+    database: "app",
+    ssl: {
+        rejectUnauthorized: true,
+    },
+})
+
+db.connect(function (err) {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
+    
+    let select = 'SELECT * FROM contact_form LIMIT 30';
+
+    db.query(select, function (err, results, fields) {
+        if (err) {
+            console.log(err.message);
+        }
+        res.json({
+          hello: results
+        });
+    });
+
+    db.end(function (err) {
+        if (err) {
+            return console.log(err.message);
+        }
+    });
+});
+  
+
 });
 
 
